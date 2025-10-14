de52fc312e7c2b93f2172e5dd14a4d0675995a53
MJLNSN <neversaynever333@outlook.com>
2025-10-14 01:04:51 -0400

    fix: try to improve ui for android browser

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -319,7 +319,7 @@ export default function UserPage() {
         {/* 头部 */}
         <div className="text-center mb-4">
           <h1 className="text-3xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-emerald-600 to-green-700 mb-1">
-            🗺️ 箭塔探索地图
+            🗺️ 箭塔村探索地图
           </h1>
           <p className="text-gray-600 font-medium">点击地图景点查看详情并打卡</p>
         </div>
